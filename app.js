f70fd2098fe71c90b44b99d30e89afc5df9502a8
Sunny Dubey <sunnydubey2502@gmail.com>
2023-08-31 23:04:04 +0530

    authentication

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const AppError = require('./utils/appError');
-const router = require('./routes/routes');
+const movieRouter = require('./routes/routes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 app.use(cors());
@@ -12,7 +13,8 @@ app.use(express.json({ limit: '10kb' }));
 app.use(morgan('dev'));
 
 // routes
-app.use('/api', router);
+app.use('/api', userRouter);
+app.use('/api', movieRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
